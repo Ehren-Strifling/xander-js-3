bd63bc85e65d54c17ab161a1f4480b7dad3ddc9c
ehren strifling <ehrenstrifling@gmail.com>
2023-11-09 10:19:31 -0600

    Nintendo Switch Controller Support

--- scripts/input-v2.js
+++ scripts/input-v2.js
@@ -5,9 +5,11 @@
 //Requires Vector2.js
 
 class InputV2 {
+  //Most browsers only support up to 4 controllers but there is no defined limit
+  static CONTROLLER_COUNT = 4;
   constructor() {
     /**@type {InputV2Controller[]} */
-    this.controllers = new Array(4);
+    this.controllers = new Array(this.constructor.CONTROLLER_COUNT);
     for (let i=0;i<this.controllers.length;++i) {
       this.controllers[i] = new InputV2Controller();
     }
@@ -20,13 +22,27 @@ class InputV2 {
     this.keyboardManager = new InputV2KeyboardManager();
     /**@type {InputV2MouseManager} */
     this.mouseManager = new InputV2MouseManager();
+
+    /**@type {InputV2GamepadManager} */
+    this.gamepadManager = new InputV2GamepadManager();
   }
 
+  act() {
+    this.manageController();
+  }
   reset() {
     this.keyboardManager.reset();
     this.mouseManager.reset();
     for (let i=0;i<this.controllers.length;++i) {
       this.controllers[i].reset();
+    }
+  }
+
+  manageController() {
+    let gamepads = navigator.getGamepads();
+    let count = Math.min(gamepads.length, this.constructor.CONTROLLER_COUNT);
+    for (let i=0;i<count;++i) {
+      this.gamepadManager.update(gamepads[i], this.controllers[i]);
     }
   }
 
@@ -82,8 +98,8 @@ class InputV2 {
 class InputV2Controller {
   //these are supposed to be used inside this script
   static BUTTONS = Object.freeze({
-    START:              0,
-    SELECT:             1,
+    SELECT:             0,
+    START:              1,
     A_BUTTON:           2,
     B_BUTTON:           3,
     C_BUTTON:           4,
@@ -96,10 +112,14 @@ class InputV2Controller {
     RIGHT_BUTTON:       11,
     LEFT_TRIGGER:       12,
     RIGHT_TRIGGER:      13,
-    LEFT_STICK:         14,
-    RIGHT_STICK:        15,
-    HOME:               16,
-    SCREEN_SHOT:        17
+    DPAD_UP:            14,
+    DPAD_DOWN:          15,
+    DPAD_LEFT:          16,
+    DPAD_RIGHT:         17,
+    LEFT_STICK:         18,
+    RIGHT_STICK:        19,
+    HOME:               20,
+    SCREEN_SHOT:        21
   });
 
   static AXIS = Object.freeze({
@@ -114,13 +134,13 @@ class InputV2Controller {
   static NONE = 0;
 
   constructor() {
-    this.buttons = new Array(18);
+    this.buttons = new Array(22);
     this.buttons.fill(0)
-    this.analogs = new Array(18);
+    this.analogs = new Array(22);
     this.analogs.fill(0);
-    this.axis = new Array(2);
-    for (let i=0;i<this.axis.length;++i) {
-      this.axis[i] = new Vector2();
+    this.axes = new Array(2);
+    for (let i=0;i<this.axes.length;++i) {
+      this.axes[i] = new Vector2();
     }
   }
 
@@ -131,11 +151,11 @@ class InputV2Controller {
   }
 
   //Button getters
+  get buttonSelect() {
+    return this.buttons[this.constructor.BUTTONS.SELECT];
+  }
   get buttonStart() {
     return this.buttons[this.constructor.BUTTONS.START];
-  }
-  get buttonSelect() {
-    return this.buttons[this.constructor.BUTTONS>SELECT];
   }
   get buttonA() {
     return this.buttons[this.constructor.BUTTONS.A_BUTTON];
@@ -179,6 +199,18 @@ class InputV2Controller {
   get buttonRS() {
     return this.buttons[this.constructor.BUTTONS.RIGHT_STICK];
   }
+  get buttonDPadUp() {
+    return this.buttons[this.constructor.BUTTONS.DPAD_UP];
+  }
+  get buttonDPadDown() {
+    return this.buttons[this.constructor.BUTTONS.DPAD_UP];
+  }
+  get buttonDPadLeft() {
+    return this.buttons[this.constructor.BUTTONS.DPAD_LEFT];
+  }
+  get buttonDPadRight() {
+    return this.buttons[this.constructor.BUTTONS.DPAD_RIGHT];
+  }
   get buttonHome() {
     return this.buttons[this.constructor.BUTTONS.HOME];
   }
@@ -186,11 +218,11 @@ class InputV2Controller {
     return this.buttons[this.constructor.BUTTONS.SCREEN_SHOT];
   }
 
+  set buttonSelect(n) {
+    this.buttons[this.constructor.BUTTONS.SELECT] = n;
+  }
   set buttonStart(n) {
     this.buttons[this.constructor.BUTTONS.START] = n;
-  }
-  set buttonSelect(n) {
-    this.buttons[this.constructor.BUTTONS>SELECT] = n;
   }
   set buttonA(n) {
     this.buttons[this.constructor.BUTTONS.A_BUTTON] = n;
@@ -234,6 +266,18 @@ class InputV2Controller {
   set buttonRS(n) {
     this.buttons[this.constructor.BUTTONS.RIGHT_STICK] = n;
   }
+  set buttonDPadUp(n) {
+    this.buttons[this.constructor.BUTTONS.DPad_UP] = n;
+  }
+  set buttonDPadDown(n) {
+    this.buttons[this.constructor.BUTTONS.DPad_DOWN] = n;
+  }
+  set buttonDPadLeft(n) {
+    this.buttons[this.constructor.BUTTONS.DPad_LEFT] = n;
+  }
+  set buttonDPadRight(n) {
+    this.buttons[this.constructor.BUTTONS.DPad_RIGHT] = n;
+  }
   set buttonHome(n) {
     this.buttons[this.constructor.BUTTONS.HOME] = n;
   }
@@ -242,11 +286,11 @@ class InputV2Controller {
   }
 
   //analog input getters. Most of these unused
+  get analogSelect() {
+    return this.analogs[this.constructor.BUTTONS.SELECT];
+  }
   get analogStart() {
     return this.analogs[this.constructor.BUTTONS.START];
-  }
-  get analogSelect() {
-    return this.analogs[this.constructor.BUTTONS>SELECT];
   }
   get analogA() {
     return this.analogs[this.constructor.BUTTONS.A_BUTTON];
@@ -271,7 +315,7 @@ class InputV2Controller {
   }
   get analogZ() {
     return this.analogs[this.constructor.BUTTONS.Z_BUTTON];
-  }
+  }pressed
   get analogLB() {
     return this.analogs[this.constructor.BUTTONS.LEFT_BUTTON];
   }
@@ -290,6 +334,18 @@ class InputV2Controller {
   get analogRS() {
     return this.analogs[this.constructor.BUTTONS.RIGHT_STICK];
   }
+  get analogDPadUp() {
+    return this.analogs[this.constructor.BUTTONS.DPAD_UP];
+  }
+  get analogDPadDown() {
+    return this.analogs[this.constructor.BUTTONS.DPAD_UP];
+  }
+  get analogDPadLeft() {
+    return this.analogs[this.constructor.BUTTONS.DPAD_LEFT];
+  }
+  get analogDPadRight() {
+    return this.analogs[this.constructor.BUTTONS.DPAD_RIGHT];
+  }
   get analogHome() {
     return this.analogs[this.constructor.BUTTONS.HOME];
   }
@@ -297,11 +353,11 @@ class InputV2Controller {
     return this.analogs[this.constructor.BUTTONS.SCREEN_SHOT];
   }
 
+  set analogSelect(n) {
+    this.analogs[this.constructor.BUTTONS.SELECT] = n;
+  }
   set analogStart(n) {
     this.analogs[this.constructor.BUTTONS.START] = n;
-  }
-  set analogSelect(n) {
-    this.analogs[this.constructor.BUTTONS>SELECT] = n;
   }
   set analogA(n) {
     this.analogs[this.constructor.BUTTONS.A_BUTTON] = n;
@@ -345,6 +401,18 @@ class InputV2Controller {
   set analogRS(n) {
     this.analogs[this.constructor.BUTTONS.RIGHT_STICK] = n;
   }
+  set analogDPadUp(n) {
+    this.analogs[this.constructor.BUTTONS.DPad_UP] = n;
+  }
+  set analogDPadDown(n) {
+    this.analogs[this.constructor.BUTTONS.DPad_DOWN] = n;
+  }
+  set analogDPadLeft(n) {
+    this.analogs[this.constructor.BUTTONS.DPad_LEFT] = n;
+  }
+  set analogDPadRight(n) {
+    this.analogs[this.constructor.BUTTONS.DPad_RIGHT] = n;
+  }
   set analogHome(n) {
     this.analogs[this.constructor.BUTTONS.HOME] = n;
   }
@@ -354,11 +422,11 @@ class InputV2Controller {
 
   /**@returns {Vector2} */
   get axisLeft() {
-    return this.axis[this.constructor.AXIS.LEFT];
+    return this.axes[this.constructor.AXIS.LEFT];
   }
   /**@returns {Vector2} */
   get axisRight() {
-    return this.axis[this.constructor.AXIS.RIGHT];
+    return this.axes[this.constructor.AXIS.RIGHT];
   }
 }
 
@@ -490,3 +558,158 @@ class DefaultMouseManager extends InputV2MouseManager {
     }
   };
 }
+
+class InputV2GamepadManager {
+  constructor() {
+    this.lastUpdated = 0;
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2} controller 
+   */
+  update(gamepad, controller) {
+    if (gamepad===null) {
+      return this.disconnected();
+    }
+    if (gamepad.timestamp<=this.lastUpdated) {
+      return;
+    }
+    let id = gamepad.id;
+    if (id.indexOf("057e")>-1) {
+      this.nsProUpdate(gamepad, controller);
+    } else {
+      this.genericUpdate(gamepad, controller);
+    }
+    this.lastUpdated = gamepad.timestamp;
+  }
+  disconnected() {
+
+  }
+
+  /** Almost every controller will have the same joysticks so use this method to deal with them.
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  genericJoystick(gamepad, controller) {
+    let axisLength = Math.min(gamepad.axes.length,4);
+    for (let i=0; i<axisLength;++i) {
+      let a = Math.floor(i/2);
+      if (i&1) {
+        controller.axes[a].y = gamepad.axes[i];
+      } else {
+        controller.axes[a].x = gamepad.axes[i];
+      }
+    }
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  genericABXY(gamepad, controller, buttonIndex = 0) {
+    //normally bottom, right, left, top
+    //It took a lot of thinking to create this equation and I'm quite happy with it.
+    //I'm so glad I don't have to spam if statements now.
+    controller.buttonA = ((controller.buttonA>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogA = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonB = ((controller.buttonB>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogB = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonX = ((controller.buttonX>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogX = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonY = ((controller.buttonY>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogY = gamepad.buttons[buttonIndex].value; 
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  genericLBRB(gamepad, controller, buttonIndex = 4) {
+    controller.buttonLB = ((controller.buttonLB>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogLB = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonRB = ((controller.buttonRB>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogRB = gamepad.buttons[buttonIndex].value; 
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  genericLTRT(gamepad, controller, buttonIndex) {
+    controller.buttonLT = ((controller.buttonLT>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogLT = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonRT = ((controller.buttonRT>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogRT = gamepad.buttons[buttonIndex].value; 
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  genericSelectStart(gamepad, controller, buttonIndex) {
+    controller.buttonSelect = ((controller.buttonSelect>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogSelect = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonStart = ((controller.buttonStart>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogStart = gamepad.buttons[buttonIndex].value; 
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  genericLSRS(gamepad, controller, buttonIndex) {
+    controller.buttonLS = ((controller.buttonLS>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogLS = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonRS = ((controller.buttonRS>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogRS = gamepad.buttons[buttonIndex].value; 
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  genericDPad(gamepad, controller, buttonIndex) {
+    //up down left right
+    controller.buttonDPadUp = ((controller.buttonDPadUp>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogDPadUp = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonDPadDown = ((controller.buttonDPadDown>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogDPadDown = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonDPadLeft = ((controller.buttonDPadLeft>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogDPadLeft = gamepad.buttons[buttonIndex].value; 
+    buttonIndex++;
+    controller.buttonDPadRight = ((controller.buttonDPadRight>>>1) ^ gamepad.buttons[buttonIndex].pressed*3);
+    controller.analogDPadRight = gamepad.buttons[buttonIndex].value; 
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  genericUpdate(gamepad, controller) {
+    this.genericJoystick(gamepad, controller);
+    this.genericABXY(gamepad, controller);
+    this.genericLBRB(gamepad, controller);
+  }
+  /**
+   * @param {Gamepad} gamepad 
+   * @param {InputV2Controller} controller 
+   */
+  nsProUpdate(gamepad, controller) {
+    this.genericJoystick(gamepad, controller);
+    this.genericABXY(gamepad, controller);
+    this.genericLBRB(gamepad, controller);
+    this.genericLTRT(gamepad, controller, 6)
+    this.genericSelectStart(gamepad, controller, 8)
+    this.genericLSRS(gamepad, controller, 10);
+    this.genericDPad(gamepad, controller, 12);
+    
+    controller.buttonHome = ((controller.buttonHome>>>1) ^ gamepad.buttons[16].pressed*3);
+    controller.analogHome = gamepad.buttons[16].value; 
+
+    controller.buttonScreenShot = ((controller.buttonScreenShot>>>1) ^ gamepad.buttons[17].pressed*3);
+    controller.analogScreenShot = gamepad.buttons[17].value; 
+  }
+}
+
